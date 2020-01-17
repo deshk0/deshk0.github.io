4dfa9dd37386793a8331a34fe74c12b50088c3f1
deshk0 <50766489+deshk0@users.noreply.github.com>
2020-01-17 17:25:42 +0200

    Add files via upload

---
+++ build/precache-manifest.820cb5632fd2ceedb76f08a60a670d02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df113b693e7099e27e71020afe62f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8220b8cf3264cee6e81",
+    "url": "/static/js/2.fbd098d8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fbd098d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bb333a3a9b079eac823",
+    "url": "/static/js/main.8910d7a5.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
